165703561638afb8e02b8056633e63218e24518c
Ciro Ribeiro <36665421+cirosr@users.noreply.github.com>
2022-12-29 17:51:24 +0000

    Add files via upload

---
+++ Oracle DB Hierarchical Query.sql
@@ -0,0 +1,9 @@
+-- Oracle DB, mainly EBS, to perform similar operation as PATH() in DAX:
+
+SELECT last_name, employee_id, manager_id, LEVEL
+      FROM employees
+      START WITH employee_id = 100
+      CONNECT BY PRIOR employee_id = manager_id
+      ORDER SIBLINGS BY last_name;
+
+
